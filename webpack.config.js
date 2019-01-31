b48fe55953f8249a6bcd8740c8be6ccedaa11050
finch <mobai@douyu.tv>
2019-01-31 14:53:15 +0800

    will this workd

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /nodue_moduel/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin("dist", {}),
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html"
+    })
+  ]
+};
